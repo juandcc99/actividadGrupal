fc71872dc452c12f1be8825d3496ed224bd1dd54
juandcc99 <93380707+juandcc99@users.noreply.github.com>
2022-03-18 14:38:13 +0100

    Create cabecera.php

---
+++ Views/Layouts/cabecera.php
@@ -0,0 +1,29 @@
+	
+		<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+  			<div class="container-fluid">
+  				<a class="navbar-brand" href="#"><img src=""></a>
+  				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+      				<span class="navbar-toggler-icon"></span>
+    			</button>
+    			<div class="collapse navbar-collapse" id="collapsibleNavbar">
+      				<ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php"><i class="fas fa-home"> Inicio</i></a>
+                        </li>
+        				<li class="nav-item dropdown">
+          					<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"> Paginas</a>
+          					<ul class="dropdown-menu bg-light">
+            					<li><a class="dropdown-item" href="?controller=alumno&action=register"> Registrar</a></li>
+                                <li><a class="dropdown-item" href="?controller=alumno&action=show"> Ver</a></li>
+          					</ul>
+        				</li>
+        				<li class="nav-item">
+          					<a class="nav-link" href="https://github.com/juandcc99/"><i class="fab fa-github"> GitHub</i></a>
+        				</li> 
+        				<li class="nav-item">
+          					<a class="nav-link" href="?controller=alumno&action=contacto"><i class="fas fa-envelope"> Contactos</i></a>
+        				</li> 
+      				</ul>
+    			</div>
+  			</div>
+		</nav>
